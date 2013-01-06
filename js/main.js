94400b96cbbde70dd532d1a3e33f33e551e53f2a
vlad.pupaza <vlad.pupaza@softvision.ro>
2013-01-06 21:02:11 +0200

    main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+require.config({
+	baseUrl:'../',
+	paths: {
+		jquery: 'libs/jquery/jquery-1.8.3',
+		backbone: 'libs/backbone/backbone',
+		underscore: 'libs/underscore/underscore',
+		text: 'libs/require/text'
+	}
+});
