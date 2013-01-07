c0712b2850fad456519770a0123e8bfe252620b4
vlad.pupaza <vlad.pupaza@softvision.ro>
2013-01-07 18:00:51 +0200

    template test3

--- js/modules/appViews/appViewTemplate.js
+++ js/modules/appViews/appViewTemplate.js
@@ -1,34 +1,36 @@
-// Toolbar Template 
-<script type="text/template" id="toolbar_buttons">
-  	<button type="button" id="addSlideBtn">ADD SLIDE</button>
-	<button type="button" id="removeSlideBtn">Remove SLIDE</button>
-	<button type="button" id="addImageToSlideBtn">ADD IMAGE</button>
-	<button type="button" id="removeImageFromSlideBtn">REMOVE IMAGE</button>
-	<button type="button" id="addVideoBtn">Add Video</button>
-	<button type="button" id="removeVideoBtn">Remove Video</button>
-	<button type="button" id="saveBtn">SAVE</button>
-	<div id="languageDtopdown">
-		<select id = "languageOption">
-			<option value="english" selected="selected">English</option>
-			<option value="romanian">Romanian</option>
-		</select>
-	</div>
-</script>
+var appViewTemplate = function(){
 
-// Typebar Template
-<script type="text/template" id="typebar_template">
-	<div class="slideType currentSlideType" id="typeText">
-		<p>type text</p>
-	</div>
-	<div class="slideType" id="typeImage">
-		<p>type image</p>
-	</div>
-	<div class="slideType" id="typeVideo">
-		<p>type video</p>
-	</div>
-</script>
+	// Toolbar Template 
+	var toolbar_buttons : ''  
+  	+'<button type="button" id="addSlideBtn">ADD SLIDE</button>'
+	+'<button type="button" id="removeSlideBtn">Remove SLIDE</button>'
+	+'<button type="button" id="addImageToSlideBtn">ADD IMAGE</button>'
+	+'<button type="button" id="removeImageFromSlideBtn">REMOVE IMAGE</button>'
+	+'<button type="button" id="addVideoBtn">Add Video</button>'
+	+'<button type="button" id="removeVideoBtn">Remove Video</button>'
+	+'<button type="button" id="saveBtn">SAVE</button>'
+	+'<div id="languageDtopdown">'
+		+'<select id = "languageOption">'
+			+'<option value="english" selected="selected">English</option>'
+			+'<option value="romanian">Romanian</option>'
+		+'</select>'
+	+'</div>',
 
-// Content Template
-<script type="text/template" id="content_template">
-	<div id="slideWorkArea"></div>	
-</script>
+
+	// Typebar Template
+	var typebar_template : '' 
+	+'<div class="slideType currentSlideType" id="typeText">'
+		+'<p>type text</p>'
+	+'</div>'
+	+'<div class="slideType" id="typeImage">'
+		+'<p>type image</p>'
+	+'</div>'
+	+'<div class="slideType" id="typeVideo">'
+		+'<p>type video</p>'
+	+'</div>',
+
+
+	// Content Template
+	var content_template : '<div id="slideWorkArea"></div>'
+}()	
+	
