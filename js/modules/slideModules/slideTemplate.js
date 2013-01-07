e83843dfcb69c3e51e664688ddea36ec3e769d96
vlad.pupaza <vlad.pupaza@softvision.ro>
2013-01-07 17:41:11 +0200

    test template2

--- js/modules/slideModules/slideTemplate.js
+++ js/modules/slideModules/slideTemplate.js
@@ -1,24 +1,26 @@
-// SlideLittle Template
-var slide_little = '<div class="slideLittle"></div>'
+var slideTemplate = function(){
+	// SlideLittle Template
+	var slide_little : '<div class="slideLittle"></div>',
 	
 
-// SlideText Template
-var slide_text = '<div class="slideText"></div>'
+	// SlideText Template
+	var slide_text : '<div class="slideText"></div>',
 	
 
 
-// SlideVideo Template
-var slide_video = '' +
-	'<div class="slideVideo">'
-		+'<video width="320" height="240" controls>'
-  			+'<source src="movie.ogg" type="video/ogg">'
-			+'Your browser does not support the video tag.'
-		+'</video>'
-	+'</div>'
+	// SlideVideo Template
+	var slide_video : ''
+		+'<div class="slideVideo">'
+			+'<video width="320" height="240" controls>'
+  				+'<source src="movie.ogg" type="video/ogg">'
+				+'Your browser does not support the video tag.'
+			+'</video>'
+		+'</div>',
 
 
-// SlideImage Template
-var slide_img = ''+
-	'<div class="slideImg">'
-		+'<img src="" alt="image" />'
-	+'</div>'
+	// SlideImage Template
+	var slide_img : ''+
+		'<div class="slideImg">'
+			+'<img src="" alt="image" />'
+		+'</div>'
+}()
