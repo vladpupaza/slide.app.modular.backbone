3d5b522ab41edb7a74de474ae96ae4b537775cad
claudia.stefan <claudia.stefan@softvision.ro>
2013-01-07 16:40:10 +0200

    v2

---
+++ js/modules/appViews/toolbarView.js
@@ -0,0 +1,29 @@
+ToolbarView = Backbone.View.extend({
+initialize: function(){
+            this.render();
+          },
+render: function(){
+//...........NUMLELE TEMPLETAULUI TREBUIE SCHIMBAT.........
+            var template = _.template( $("#toolbar_buttons").html(), {} );
+            this.$el.html( template );
+          },
+events: {
+    "click #addSlideBtn" : "addSlide",
+    "click #removeSlideBtn" : "removeSlide",
+    "click #addImageToSlideBtn" : "addImage",
+    "click #removeImageFromSlideBtn" : "removeImage",
+    "click #addVideoBtn" : "addVideo",
+    "click #removeVideoBtn" : "removeVideo",
+    "click #saveBtn" : "save",
+    "change #languageOption ": "selectLanguage"        
+},
+addSlide : function(){ alert("add slide");},
+removeSlide : function() { alert("remove slide");},
+addImage : function() { alert("add image");},
+removeImage : function(){ alert("remove Image");},
+addVideo : function(){ alert(" add Video");},
+removeVideo : function(){alert("remove Video");},
+save: function(){alert("save");},
+selectLanguage: function(e){alert("Select:"+$(e.currentTarget).val()); }
+});
+ 
