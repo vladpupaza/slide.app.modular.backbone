74afb00ac2bcb07e90a096c14378231136f30e1a
vlad.pupaza <vlad.pupaza@softvision.ro>
2013-01-07 16:59:43 +0200

    test template

--- js/modules/slideModules/slideTemplate.js
+++ js/modules/slideModules/slideTemplate.js
@@ -1,26 +1,24 @@
 // SlideLittle Template
-<script type="text/template" id="slide_little">
-	<div class="slideLittle"></div>
-</script>
+var slide_little = '<div class="slideLittle"></div>'
+	
 
 // SlideText Template
-<script type="text/template" id="slide_text">
-	<div class="slideText"></div>
-</script>
+var slide_text = '<div class="slideText"></div>'
+	
+
 
 // SlideVideo Template
-<script type="text/template" id="slide_video">
-	<div class="slideVideo">
-		<video width="320" height="240" controls>
-  			<source src="movie.ogg" type="video/ogg">
-			Your browser does not support the video tag.
-		</video>
-	</div>
-</script>
+var slide_video = '' +
+	'<div class="slideVideo">'
+		+'<video width="320" height="240" controls>'
+  			+'<source src="movie.ogg" type="video/ogg">'
+			+'Your browser does not support the video tag.'
+		+'</video>'
+	+'</div>'
+
 
 // SlideImage Template
-<script type="text/template" id="slide_img">
-	<div class="slideImg">
-		<img src="" alt="image" />
-	</div>
-</script>
+var slide_img = ''+
+	'<div class="slideImg">'
+		+'<img src="" alt="image" />'
+	+'</div>'
