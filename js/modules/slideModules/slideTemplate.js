6dd560d97d99fa190035b8600f3b6a4e2104eb44
vlad.pupaza <vlad.pupaza@softvision.ro>
2013-01-08 11:30:50 +0200

    changes to style.css, templates

--- js/modules/slideModules/slideTemplate.js
+++ js/modules/slideModules/slideTemplate.js
@@ -1,29 +1,25 @@
 define([], function() {
 var slideTemplate = {
- // SlideLittle Template
-  slide_little : '<div class="slideLittle"></div>',
- 
-
  // SlideText Template
-  slide_text : '<div class="slideText"></div>',
+  slide_text : '<div class="slideLittle"><div class="slideText"></div></div>',
  
 
 
  // SlideVideo Template
   slide_video : ''
-  +'<div class="slideVideo">'
+  +'<div class="slideLittle"><div class="slideVideo">'
    +'<video width="320" height="240" controls>'
       +'<source src="movie.ogg" type="video/ogg">'
     +'Your browser does not support the video tag.'
    +'</video>'
-  +'</div>',
+  +'</div></div>',
 
 
  // SlideImage Template
   slide_img : ''+
-  '<div class="slideImg">'
+  '<div class="slideLittle"><div class="slideImg">'
    +'<img src="" alt="image" />'
-  +'</div>',
+  +'</div></div>',
 
 };
 return slideTemplate;
