b4777d7856a11c2a690e566aeed04f694470c4b2
alexandra vilcan <alexandra.vilcan@softvision.ro>
2013-01-08 16:13:38 +0200

    rerender

--- js/modules/slideModules/slides.js
+++ js/modules/slideModules/slides.js
@@ -1,12 +1,13 @@
 define(['underscore', 'backbone','js/modules/slideModules/slide'], function(_, Backbone,slide) {
 	var Slides = Backbone.Collection.extend({
 		model:slide,
-	
+		
 		addSlide: function(){
+			var that=this;
 			require(['js/modules/slideModules/slide','js/modules/appViews/contentView'], function(slide,contentView){
 			var sl = new slide;
 			sl.setType(t.getCurrentType());
-			slides.add(sl);
+			slide.add(sl);
 			});
 			
 			
