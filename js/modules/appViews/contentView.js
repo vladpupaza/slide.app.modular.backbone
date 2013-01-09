845b26c59700650d6141beaa3bd7e1b319bff22b
Cosmin <cosmin.malutan@softvision.ro>
2013-01-09 18:40:37 +0200

    cosmin

--- js/modules/appViews/contentView.js
+++ js/modules/appViews/contentView.js
@@ -20,17 +20,18 @@ var contentView = Backbone.View.extend({
 		else if(this.model.toJSON()._type=='Text')this.template=_.template(Template.content_template_text);
 		else if(this.model.toJSON()._type=='Video')this.template=_.template(Template.content_template_video);
 		else console.log('I don;t have a type');
-		window.curent_slide=this.model;
-        _.bindAll(this, "render"); 
-		this.model.bind('change',this.superCall); 
- 
-			if(this.model.get('id')!=null){ this.render();
-		    this.addListeners();}
+		_.bindAll(this, "render");  
+		
+		this.model.bind('change',this.render); 
+		if(window.currentSlide){window.currentSlide.bind('change',this.render)};
+		if(this.model.get('id')!=null){ 
+			this.render(); 
+		}
  
     },
-    render: function () {
-		
+    render: function () { 
         this.el.html(this.template(this.model.toJSON()));
+		this.addListeners();
         return this;
     },
 	alertMe:function(){console.log('I"m here');},
@@ -45,12 +46,10 @@ var contentView = Backbone.View.extend({
 	{	 
 		document.getElementById('slideWorkArea').removeEventListener('mousemove', divMove, true);
 		setTimeout(function(){
-			curent_slide.set({_y:$('#draggebel').css('top')});
-			curent_slide.set({_x:$('#draggebel').css('left')}); },200);
+			currentSlide.set({_y:$('#draggebel').css('top')},{silent: true});
+			currentSlide.set({_x:$('#draggebel').css('left')}); },200);
+
  
-	},
-	superCall:function(){
-		this.initialize(); 
 	},
 	
 	mouseDown:function (e){
