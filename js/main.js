8ff3503e6853729121e5e182a3e7ccf66d9ea176
vlad.pupaza <vlad.pupaza@softvision.ro>
2013-01-09 09:20:05 +0200

    add order.js

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ require.config({
 		jquery: 'js/libs/jquery/jquery-1.8.3',
 		underscore: 'js/libs/underscore/underscore-min',
 		backbone: 'js/libs/backbone/backbone-optamd3-min',
-		localStorage: 'js/libs/backbone/localstorage'
-		
+		localStorage: 'js/libs/backbone/localstorage',
+    order: 'js/libs/require/order'
 	}
 });
 
