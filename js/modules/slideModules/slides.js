462fed737df73573c9e2b94b28469e8cf8a44c2e
Cosmin <cosmin.malutan@softvision.ro>
2013-01-09 10:34:31 +0200

    cosmin

--- js/modules/slideModules/slides.js
+++ js/modules/slideModules/slides.js
@@ -4,6 +4,8 @@ define(['underscore', 'backbone','js/modules/slideModules/slide','localStorage']
 		model:slide,/* 
 		url:'slide', */
 		localStorage:new Store('cosmin_Slides'),
+		
+		
 		addSlide: function(){
 			var that=this;
 			require(['js/modules/slideModules/slide','js/modules/appViews/contentView'], function(slide,contentView){
