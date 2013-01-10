5dad9363418fb7ec216267bd25c992f63acf5b51
unknown <daniel.panc@softvision.ro>
2013-01-10 12:07:47 +0200

    bug fix and comments

--- js/modules/slideModules/slides.js
+++ js/modules/slideModules/slides.js
@@ -3,10 +3,11 @@ define(['underscore', 'backbone','js/modules/slideModules/slide','localStorage',
 	var Slides = Backbone.Collection.extend({
         initialize : function(){
          var t = new slide;
+		 // we bind the event on slide (subscribe), here,  only once in all application
          var token= pubSub.subscribe('getUrl', t.subscriber); 
         },
-		model:slide,/* 
-		url:'slide', */
+		model:slide,
+		//local storage usingBlackbone localstorage
 		localStorage:new Store('cosmin_Slides'),
 		
 		updateAll: function() {
@@ -15,14 +16,13 @@ define(['underscore', 'backbone','js/modules/slideModules/slide','localStorage',
 				
 			}
 		},
-		
+		// add slide function
+		//here we simulate a singleton: we have one single instance of slides collection and we add slides only on it, even if we call addSlide function from another instance of slides collection
 		addSlide: function(){
 			var that=this;
 			require(['js/modules/slideModules/slide','js/modules/appViews/contentView'], function(slide,contentView){
 			var sl = new slide;
 			sl.setType(typeViewObj.getCurrentType());
- 
-
 			slides.add(sl);
 			sl.save();
 			console.log("POST ../slides");
