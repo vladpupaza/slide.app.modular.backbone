ac6746ce878a9290c3781f41d0e9a60d3b017a1c
vlad.pupaza <vlad.pupaza@softvision.ro>
2013-01-14 10:03:57 +0200

    jshint validation on slide.js, appView.js, main.js

--- js/main.js
+++ js/main.js
@@ -3,27 +3,26 @@
 require.config({
 /** Sets the base url
  */
-  baseUrl:'',
+	baseUrl:'',
 /** Here we define the paths to jquery, underscore, backbone, 
  * localstorage and order.........................................
  */ 
-  paths: {
-    jquery: 'js/libs/jquery/jquery-1.8.3',
-    underscore: 'js/libs/underscore/underscore-min',
-    backbone: 'js/libs/backbone/backbone-optamd3-min',
-    localStorage: 'js/libs/backbone/localstorage',
-    order: 'js/libs/require/order',
-   }
+	paths: {
+	jquery: 'js/libs/jquery/jquery-1.8.3',
+	underscore: 'js/libs/underscore/underscore-min',
+	backbone: 'js/libs/backbone/backbone-optamd3-min',
+	localStorage: 'js/libs/backbone/localstorage',
+	order: 'js/libs/require/order'
+	}
 });
 
- 
 require(['js/modules/appViews/appView'], function(AppView){
 /** Sets the idCurrent variable to -1 so that we start the app with no selected slide
  */
-  window.idCurrent = -1;
+	window.idCurrent = -1;
 /** We create a appView object that is used to render the app
  * @property
  * @type object
  */
-  var appViewObj = new AppView(); 
+	var appViewObj = new AppView(); 
 });
