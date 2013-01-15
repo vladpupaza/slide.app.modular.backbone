4aa0556d0dc52ce2debd6b442e51f3f5b1d37cf9
vlad.pupaza <vlad.pupaza@softvision.ro>
2013-01-15 09:53:49 +0200

    format changes and copyright

--- js/modules/appViews/appView.js
+++ js/modules/appViews/appView.js
@@ -1,49 +1,49 @@
 define([
-	'jquery',
-	'underscore',
-	'backbone',
-	'js/modules/slideModules/slideModules',
-	'js/modules/appViews/toolbarView',
-	'js/modules/appViews/sidebarView',
-	'js/modules/appViews/contentView',
-	'js/modules/appViews/typeView'
-	],
+    'jquery',
+    'underscore',
+    'backbone',
+    'js/modules/slideModules/slideModules',
+    'js/modules/appViews/toolbarView',
+    'js/modules/appViews/sidebarView',
+    'js/modules/appViews/contentView',
+    'js/modules/appViews/typeView'
+    ],
 
 function($, _, Backbone,SlideModules,Toolbar, Sidebar, Content, Type){
 /**
  * @class AppView A View representing the UI
  * @constructor
- */	
-	var AppView = function(){
+ */ 
+    var AppView = function(){
 /** 
  * @property
  * @type object
  */
-		typeViewObj	= new Type();	
+        typeViewObj = new Type();   
 /** 
  * @property
  * @type object
  */
-		slideModulesObj = new SlideModules();
+        slideModulesObj = new SlideModules();
 /** 
  * @property
  * @type object
  */
-		toolbarViewObj = new Toolbar();
+        toolbarViewObj = new Toolbar();
 /**
  * @method
  * @param {exception} e An exception
- */		
-		divMove = function (e){	
-			var div = document.getElementById('draggebel'); 
-			div.style.top = ( e.clientY -300)+ 'px';
-			div.style.left = ( e.clientX -950)+ 'px';
-		};
+ */     
+        divMove = function (e) {    
+            var div = document.getElementById('draggebel'); 
+            div.style.top = ( e.clientY -300)+ 'px';
+            div.style.left = ( e.clientX -950)+ 'px';
+        };
 /** 
  * @property
  * @type object
  */
-		sidebarViewObj = new Sidebar({collection:slideModulesObj.slides});
-	};
-	return AppView;
+        sidebarViewObj = new Sidebar({collection:slideModulesObj.slides});
+    };
+    return AppView;
 });
--- js/modules/slideModules/slide.js
+++ js/modules/slideModules/slide.js
@@ -3,47 +3,49 @@ define(['underscore', 'backbone'], function(_, Backbone) {
  * @class Slide A Slide Model
  * @extends Backbone.Model
  * @constructor
- */	
-	"use strict";
-	var Slide = Backbone.Model.extend({
-		defaults: {
+ */ 
+    "use strict";
+    var Slide = Backbone.Model.extend ( {
+        defaults: {
 /** 
  * @property
  * @type string
  */
-			'id':null,
+            'id': null,
 /** 
  * @property
  * @type string
  */
-			"_type" : 'Image',
+            "_type": 'Image',
 /** 
  * @property
  * @type string
- */			
-			"_text" : null,
+ */         
+            "_text": null,
 /** 
  * @property
  * @type string
- */			
-			"_url" :null,
+ */         
+            "_url": null,
 /** 
  * @property
  * @type string
- */			 
-			'_x':'150',
+ */          
+            '_x': '150',
 /** 
  * @property
  * @type string
- */			
-			'_y':'50'
-		},
+ */         
+            '_y': '50'
+        },
 /**
  * @method
  */
-		initialize : function(){
-			if (this.id===null) {this.set({id:Math.random().toString(36).substr(2,9)});}
-		}
-	});
-	return Slide;
+        initialize: function () {
+            if (this.id === null) {
+                this.set({id:Math.random().toString(36).substr(2,9)});
+            }
+        }
+    });
+    return Slide;
 });
