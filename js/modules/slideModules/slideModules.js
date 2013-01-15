b5b6e44bc48952a1ec3ab6c54a288075b5666831
radu.moldovan <radu.moldovan@softvision.ro>
2013-01-15 10:39:50 +0200

    Text formating + some more comments

--- js/modules/slideModules/slideModules.js
+++ js/modules/slideModules/slideModules.js
@@ -2,7 +2,7 @@ define([
 'js/modules/slideModules/slides',
 'js/modules/slideModules/slide',
 'js/libs/pubsub'], 
-    function (S,Slide,PubSub) {
+    function(S,Slide,PubSub) {
     // slideModule is used for making an instance of Slides collection that basicly starts the engine of the application
     /**
     *@class SlideModule The Slide Module used in the application    
@@ -10,48 +10,40 @@ define([
    */
     var SlideModule =function()
     {  
-        // we create a new Collection so we can add slides to it.
         /**
         *@property
         *@type object
-        */
- 
+        */ 
+        //we create a new Collection so we can add slides to it.
         this.slides= new S(); 
+        //the collection subscribes to events on the toolbar
         this.slides.subscribeStatements();
-        var loadFromLocalStorage =function(name)
-        {
+
+        //function that reads the entry in local storage with the name recieved as a parameter
+        var loadFromLocalStorage =function(name) {
             var localStorageItem=localStorage.getItem(name);
             console.log("GET ../slides");
             return localStorageItem;
         };
-        var presentationChanger= function(msg,data)
-        {
-            slideModulesObj.slides.reset();
-            // read the local storage and see if there's anything in there, equivalent to a GET ../slides REST operation
-            var saved=loadFromLocalStorage(data);
-            if (typeof saved==='string')
-        {
-            //if theres something in the local storage load it in our Collection of slides
-            var localSlides=JSON.parse(saved);
-            var l=localSlides.length;
-            var list=[];
-            for(var i=0;i<l;i++){
-                list.push(localSlides[i])                
-            }
-            slideModulesObj.slides.reset(list);          
-        }
 
- 
+        //function that loads the correct entry form local storage based on the data it recieves from the toolbar
+        var presentationChanger= function(msg,data) {
+            var saved;
+            var localSlides;
+            //first we empty the current collection
+            slideModulesObj.slides.reset();
+            //read the local storage and see if there's anything in there, equivalent to a GET ../slides REST operation
+            saved=loadFromLocalStorage(data);            
+            if (typeof saved==='string') {
+            //if theres something in the local storage load it in our Collection of slides
+            localSlides=JSON.parse(saved);
+            slideModulesObj.slides.reset(localSlides);
+            }
         };
-        PubSub.subscribe("change presentation",presentationChanger);
-        //instance of a slide model
-		console.log("GET ../slides");
-        /**
-        *@property
-        *@type object
-        */
-        this.a = new Slide({id:'10'});
-        this.a.set({id:null});
+
+        PubSub.subscribe("change presentation",presentationChanger);       
+        
+
     };
     return SlideModule;
 });
