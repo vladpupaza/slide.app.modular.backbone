40d46bec4ecbf4d076752bd31cf67fe8ff8f22a8
Cosmin <cosmin.malutan@softvision.ro>
2013-01-17 13:37:38 +0200

    r

--- js/modules/appViews/appModule.js
+++ js/modules/appViews/appModule.js
@@ -5,15 +5,17 @@ define([
     'js/modules/slideModules/slideModules',
     'js/modules/appViews/toolbarView',
     'js/modules/appViews/sidebarView',
+    'js/modules/appViews/contentView',
     'js/modules/appViews/typeView'
     ],
 
-function ($, _, Backbone,SlideModules,Toolbar, Sidebar, Type) {
+function ($, _, Backbone,SlideModules,Toolbar, Sidebar, Content, Type) {
 /**
  * @class AppModule A View representing the UI
  * @constructor
  */ 
     var AppModule = function () {
+	content=new Content();
 /** 
  * @property
  * @type object
@@ -38,6 +40,7 @@ function ($, _, Backbone,SlideModules,Toolbar, Sidebar, Type) {
             div.style.top = ( e.clientY -300)+ 'px';
             div.style.left = ( e.clientX -950)+ 'px';
         };
+
 /** 
  * @property
  * @type object
