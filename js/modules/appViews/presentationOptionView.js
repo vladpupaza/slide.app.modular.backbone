834603ef898b4cf66decd766fb08aee4ae956f59
alexandra vilcan <alexandra.vilcan@softvision.ro>
2013-01-18 13:57:04 +0200

    presentation view integrated

--- js/modules/appViews/presentationOptionView.js
+++ js/modules/appViews/presentationOptionView.js
@@ -14,10 +14,9 @@ function ($,_,Backbone,pubSub,templates)
 			this.loadNamesFromLocalStorage();
 		},
 		addPresentation:function(msg,data) {
-			$("#presentationOption").append('<option class="options" value='+data+'>'+data+'</option');
+			$("#presentationOption").append('<option class="options" value='+data+'>'+data+'</option>');
 			$("#presentationOption").val(data).attr("selected",true);
-			window.presentationOptionViewObj.render();
-		},
+         },
 		selectPresentation : function() {
         	if (this.el.find("#presentationOption").val() !== 'Select Presentation') {
 		    	pubSub.publish("change presentation",this.el.find("#presentationOption").val());
@@ -25,6 +24,7 @@ function ($,_,Backbone,pubSub,templates)
         },
         setCurrentPresentation:function(msg,name) {
         	pubSub.publish("change presentation",name);
+            $("#presentationOption").val(name).attr("selected",true);
         },
         loadNamesFromLocalStorage: function() {
         	var names = JSON.parse(localStorage.getItem("presentations"));
@@ -32,31 +32,16 @@ function ($,_,Backbone,pubSub,templates)
                 var count = names.length;
                 var i;
                 for (i=0;i<count;i++) {
-        		this.el.find("#presentationOption").append('<option class="options" value='+names[i]+'>'+names[i]+'</option');
+        		this.el.find("#presentationOption").append('<option class="options" value='+names[i]+'>'+names[i]+'</option>');
                 }
             }
         },
-
-        subscribeStatements:function(){
-        	pubSub.subscribe("presentationAdded",this.addPresentation);
-        	pubSub.subscribe("setCurrentPresentation",this.setCurrentPresentation); 
-        }
-
+        
+    subscribeStatements:function(){
+        pubSub.subscribe("presentationAdded",this.addPresentation),
+        pubSub.subscribe("setCurrentPresentation",this.setCurrentPresentation);
+    }
     });
+    
 	return PresentationOptionView;
 });
-
-/***** Changes in other files to make this work:
-
-toolbarView:
-	delete: line 115 "change #presentationOption ": "selectPresentation",
-			line 394 selectPresentation method
-			line 24->33 in render function
-	replace line 304 ,315:  //this.render(); 
-                			/this.el.find('#presentationOption').val(name).attr("selected",true);
-                		with :
-                			pubSub.publish("presentationAdded",name);
-     add to 295 (if user want to replace): pubSub.publish("setCurrentPresentation",name);
-appModule:
- 	add : 
- 		line 33 : presentationOptionViewObj = new presentationOptionView();*/
