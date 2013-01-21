bf586ccbea5d215488d437277e49bcd257fb7252
claudia.stefan <claudia.stefan@softvision.ro>
2013-01-21 17:05:15 +0200

    toolbar

--- js/modules/appViews/toolbarView.js
+++ js/modules/appViews/toolbarView.js
@@ -96,7 +96,28 @@ var  ToolbarView = Backbone.View.extend ({
     * @param {EventObject} e cancelUrl        
     */    
         "click #cancelImageUrlBtn" : "cancelUrl",
-    
+    /**
+    * @event click
+    * Fires when saveBtn is clicked
+    * @param {Button} this
+    * @param {EventObject} e save        
+    */    
+        "click #saveBtn" : "save",
+    /**
+    * @event click
+    * Fires when saveasBtn is clicked
+    * @param {Button} this
+    * @param {EventObject} e save        
+    */    
+        "click #saveAsBtn" : "saveAs",
+    /**
+         * @event click
+        * Fires when slideshowBtn is clicked
+        * @param {Button} this
+        * @param {EventObject} e slideshow        
+        */    
+        "click #slideshowBtn":"startSlideshow"    
+       
     },
     /**
     * @method    
@@ -239,7 +260,146 @@ var  ToolbarView = Backbone.View.extend ({
     cancelUrl: function() {
         $("#wrapper").hide();
         $("#spinner").hide();
+    },
+        /**
+    *@method 
+    * sets text to notfification bar and makes it visible
+    */  
+    sendSaveNotification: function(message) {
+       
+        $("#notifBar").html(message);
+                    $("#notifBar").css("visibility","visible");
+                    setTimeout(function hide() {
+                    $("#notifBar").css("visibility","hidden");
+                    },4000);          
+    },
+    /**
+    * @method    
+    *
+    */                
+    save : function() {
+        if(typeof window.Application.currentPresentation === 'undefined'){
+             this.saveAs(); 
+        } else {   
+            localStorage.setItem(window.Application.currentPresentation,JSON.stringify(window.Application.slideModulesObj.slides));
+            this.saveMessage(window.Application.currentPresentation);
+        }    
+    },
+    /**
+    * @method    
+    *
+    */ 
+    saveMessage: function(name){
+        var currentDate = new Date();
+        var saveString = name + " was saved at " + currentDate.getHours() + ":" + 
+        currentDate.getMinutes() + " " + "( " + currentDate.getDate() + "/" +
+        (currentDate.getMonth()+1) + "/" + currentDate.getFullYear() + " )";
+        this.sendSaveNotification(saveString);
+    },
+    /**
+    * @method    
+    *
+    */ 
+    confirmRename: function(name,presentations) {
+        if (confirm("Are you sure you want to replace this presentation?")) {
+            localStorage.setItem('presentations',JSON.stringify(presentations));
+            localStorage.setItem(name,JSON.stringify(window.Application.slideModulesObj.slides));
+            pubSub.publish("setCurrentPresentation",name);
+            this.saveMessage(name);
+        } else {
+            this.saveAs();
+        }
+    },
+    /**
+    * @method    
+    *
+    */ 
+    addNewPresentation: function(presentations,name) {
+        
+        localStorage.setItem('presentations',JSON.stringify(presentations));
+        localStorage.setItem(name,JSON.stringify(window.Application.slideModulesObj.slides));
+        pubSub.publish("presentationAdded",name);
+        this.saveMessage(name);
+    },
+    /**
+    * @method    
+    *
+    */ 
+    checkingPresentations: function(name){
+        // checks if there is something in local storage
+        var presentations = JSON.parse(localStorage.getItem("presentations"));
+        if ($.inArray(name,presentations) != -1) {
+        //if there is a presentation with the same name alrerady saved,
+        //asks for confirmation to replace it
+            this.confirmRename(name,presentations);
+        } else {
+            presentations.push(name);
+            this.addNewPresentation(presentations,name);
+            return true;
+        }
+    },
+    /**
+    * @method    
+    *
+    */ 
+    savePresentation: function(name){
+        if (name) {
+            if (localStorage.getItem("presentations")) {
+                return this.checkingPresentations(name);
+
+            } else {   
+                //if local storage is empty creates an array with presentation names 
+                //and adds the current presentation to local storage
+                var firstPresentation = [name];
+                this.addNewPresentation(firstPresentation,name);
+            }
+        }
+    },
+    /**
+    * @method    
+    *
+    */ 
+    presentationName: function(){
+        return prompt("Give the name for the presentation","untitled");
     }, 
+     /**
+    * @method    
+    *
+    */    
+    saveAs: function(){
+        var name = this.presentationName();   
+        this.savePresentation(name);        
+    },
+    startSlideshow: function() {
+        if (window.Application.slideModulesObj.slides.length === 0) {            
+            alert("No slides to be shown");
+        } else {     
+            $("#slideshowMode").css('visibility','visible');
+            this.nextSlide(0,this); 
+        }
+    },
+    /**
+    *@method
+    *@ hides slideshow notify bar
+    */
+    hideSlideshowBar: function(i,slidesLength,timer){
+        if (i === slidesLength-1) {
+            clearInterval(timer);
+            $("#slideshowMode").css('visibility','hidden');
+        }
+    },
+    /**
+    * @method
+    * @shows the next slide
+    */
+    nextSlide: function(i,that){
+        var timer = setInterval(function () {
+            window.Application.sidebarViewObj.setCurrentSlide(i);
+            window.location.href=('#/slide/'+window.Application.slideModulesObj.slides.at(i).id);
+            that.hideSlideshowBar(i,window.Application.slideModulesObj.slides.length,timer);           
+        i++;
+        },4000);
+    } 
 });
 return ToolbarView;
 });
